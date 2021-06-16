0f411e47faa1e98308c01fd4db18eb4613500049
David Zhao <david@davidzhao.com>
2021-06-16 14:44:46 -0700

    remove default values for tls

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,5 @@ mkdir -p build
 helm package livekit-server --destination build
 cd build
 AWS_REGION=us-east-1 helm s3 push --relative ./livekit-server*.tgz livekit
+
+aws cloudfront create-invalidation --distribution-id E2MJ94T12TAUZU --paths "/*"
