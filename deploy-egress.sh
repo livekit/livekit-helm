cfa87555c378ef0663cb0026484ac481eba45f24
David Colburn <xero73@gmail.com>
2022-05-19 11:57:36 -0700

    Egress (#27)
    * dump
    
    * undo lk server rename
    
    * update version

--- deploy-egress.sh
+++ deploy-egress.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 
 rm -rf build
 mkdir -p build
-helm package livekit-recorder --destination build
+helm package egress --destination build
 cd build
-AWS_REGION=us-east-1 helm s3 push --relative ./livekit-recorder*.tgz livekit
+AWS_REGION=us-east-1 helm s3 push --relative ./egress*.tgz livekit
 
 aws cloudfront create-invalidation --distribution-id E2MJ94T12TAUZU --paths "/*"
